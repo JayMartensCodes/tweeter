69cd0ba4d59264adebb5a4568a8d05f2abe0284d
Jay Martens <martensjay93@gmail.com>
2020-08-11 13:15:40 -0400

    Created the renderTweets function that takes an array of tweet data and passes it through createTweetElement, then appends it to the tweet container. Also some minor styling.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,20 +5,40 @@
  */
 
  // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+ const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-};
-
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 $(document).ready(function() {
+
+  const renderTweets = (tweets) => {
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
+    }
+  };
+
   const createTweetElement = (tweet) => {
     const tweetCreatedOn = new Date(tweet.created_at);
     const currentDate = new Date();
@@ -53,6 +73,5 @@ $(document).ready(function() {
     `);
   };
 
-  const $tweet = createTweetElement(tweetData);
-  $('#tweets-container').append($tweet);
+  renderTweets(data);
 });
