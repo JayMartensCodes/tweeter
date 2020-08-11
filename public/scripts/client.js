a8a92bf7c87f7acba4949b7dff063ae8e5df17e2
Jay Martens <martensjay93@gmail.com>
2020-08-11 18:55:51 -0400

    Added a responsive design that covers tablet / desktop resolutions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
     return div.innerHTML;
   };
 
-  const createTweetElement = (tweet) => { 
+  const createTweetElement = (tweet) => {
     return (`
     <article>
     <header>
@@ -45,7 +45,7 @@ $(document).ready(function() {
         ${tweet.user.handle}
       </div>
     </header>
-    <div class="tweet-container">
+    <div class="tweet-text-container">
       ${escape(tweet.content.text)}
     </div>
     <footer>
@@ -60,21 +60,18 @@ $(document).ready(function() {
     `);
   };
 
-  loadTweets();
-
   $('#submit-tweet-form').submit(function(event) {
     event.preventDefault();
     const tweetText = $('#tweet-text').val();
     const formData = $(this).serialize();
     if (tweetText.length > 140) {
       $('#tweet-submit-error').html("Tweet is too long!");
-      $('#tweet-submit-error').removeClass("hidden");
-    } else if (!tweetText.length) { 
+      $('#tweet-submit-error').slideDown("slow");
+    } else if (!tweetText.length) {
       $('#tweet-submit-error').html("Please enter some text to tweet!");
-      $('#tweet-submit-error').removeClass("hidden");
+      $('#tweet-submit-error').slideDown("slow");
     } else {
-      $('#tweet-submit-error').addClass("hidden");
-      $('#tweet-submit-error').html("");
+      $('#tweet-submit-error').slideUp("slow");
       $('#tweet-text').val("");
       $.ajax('/tweets/', { method: 'POST', data: formData })
         .then(function(response) {
@@ -82,4 +79,16 @@ $(document).ready(function() {
         });
     }
   });
+
+  $('#write-tweet').on('click', function() {
+    const $newTweet = $("#new-tweet")
+    if ($newTweet.is(":hidden")) {
+      $newTweet.slideDown("slow");
+      $('#tweet-text').focus();
+    } else {
+      $newTweet.slideUp("slow");
+    }
+  });
+
+  loadTweets();
 });
